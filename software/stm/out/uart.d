8f4bbb5aac4302d076abc33033c8bad8afea293d
Mohitpatel9999 <47581807+Mohitpatel9999@users.noreply.github.com>
2022-04-11 20:54:54 -0500

    Version 1 schematic and PCB layout

---
+++ software/stm/out/uart.d
@@ -0,0 +1,37 @@
+out/uart.o: src/uart.c src/uart.h \
+ libopencm3/include/libopencm3/stm32/usart.h \
+ libopencm3/include/libopencm3/cm3/common.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/9.3.1/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/features.h \
+ /Applications/ARM/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_stdint.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/9.3.1/include/stdbool.h \
+ libopencm3/include/libopencm3/stm32/memorymap.h \
+ libopencm3/include/libopencm3/stm32/l4/memorymap.h \
+ libopencm3/include/libopencm3/cm3/memorymap.h \
+ libopencm3/include/libopencm3/stm32/l4/usart.h \
+ libopencm3/include/libopencm3/stm32/common/usart_common_all.h \
+ libopencm3/include/libopencm3/stm32/common/usart_common_v2.h \
+ /Applications/ARM/arm-none-eabi/include/stdio.h \
+ /Applications/ARM/arm-none-eabi/include/_ansi.h \
+ /Applications/ARM/arm-none-eabi/include/newlib.h \
+ /Applications/ARM/arm-none-eabi/include/sys/config.h \
+ /Applications/ARM/arm-none-eabi/include/machine/ieeefp.h \
+ /Applications/ARM/arm-none-eabi/include/sys/cdefs.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/9.3.1/include/stddef.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/9.3.1/include/stdarg.h \
+ /Applications/ARM/arm-none-eabi/include/sys/reent.h \
+ /Applications/ARM/arm-none-eabi/include/_ansi.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_types.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/lock.h \
+ /Applications/ARM/arm-none-eabi/include/sys/types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /Applications/ARM/arm-none-eabi/include/machine/types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/stdio.h \
+ libopencm3/include/libopencm3/stm32/rcc.h \
+ libopencm3/include/libopencm3/stm32/l4/rcc.h \
+ libopencm3/include/libopencm3/stm32/common/rcc_common_all.h
